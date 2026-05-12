01191beca657e02e2b54b29615704f0fdc8be3e9
Nazim Uddin <nazimuddin5390@gmail.com>
2026-05-12 15:54:04 +0600

    first commit

---
+++ src/main/java/com/toplms/ToplmsApplication.java
@@ -0,0 +1,13 @@
+package com.toplms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToplmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToplmsApplication.class, args);
+    }
+
+}
